f735fdec83789543a503aefeb656580969898a36
Christina R <xsnachannell@gmail.com>
2022-07-21 00:14:56 -0700

    updates Project.js to fix typo in comment on where source code is from and updates ContactForm.js to add closing tags to input tags and update classes to classNames

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -8,18 +8,18 @@ function ContactForm() {
             <form>
             <p>
                 <label for="firstName">First Name:</label>
-                <input type="text" class="userContactInfo" id = "firstName" placeholder="Enter Your First Name" name="userFirstName">
+                <input type="text" className="userContactInfo" id = "firstName" placeholder="Enter Your First Name" name="userFirstName"></input>
             </p>
             <p>
                 <label for="lastName">Last Name:</label>
-                <input type="text" class="userContactInfo" id="lastName" placeholder="Enter Your Last Name" name="userLastName">
+                <input type="text" className="userContactInfo" id="lastName" placeholder="Enter Your Last Name" name="userLastName"></input>
             </p>
             <p>
                 <label for="userEmailAddress">Email Address:</label>
-                <input type="text" class="userContactInfo" id="userEmailAddress" placeholder="Enter your Email Address"  name="userEmailAddress">
+                <input type="text" className="userContactInfo" id="userEmailAddress" placeholder="Enter your Email Address"  name="userEmailAddress"></input>
             </p>
             <p>
-                <input type="submit" value="Submit">
+                <input type="submit" value="Submit"></input>
             </p>
             </form>
         </div>
--- src/components/Project.js
+++ src/components/Project.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Project() {
     return (
         <div>
-            {/* copies in navigation code from portfolio already created https://github.com/ChannellNumber5/CRobinsonPortfolio */}
+            {/* copies in project code from portfolio already created https://github.com/ChannellNumber5/CRobinsonPortfolio */}
             <div className="ImageAndCaption">
             {/* <a href="https://channellnumber5.github.io/CR-WeatherDashboard/" target="no_blank"><img src="./public/CR-WeatherDashboardScreenShot.png" alt="Project Screenshot 4" className="Project-Images"></img></a> */}
             <h3><strong>Weather Dashboard Application</strong></h3>
