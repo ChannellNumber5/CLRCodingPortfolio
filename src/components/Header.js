773db9f983ed1bb1cd584a73015636fec8d46202
Christina R <xsnachannell@gmail.com>
2022-07-20 22:36:00 -0700

    updates header.js and project.js with html fro previous portfolio and then adds comments for source of html from previous portfolio

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,11 @@ import React from 'react';
 function Header() {
     return (
         <div>
+            {/* copies in navigation code from portfolio already created https://github.com/ChannellNumber5/CRobinsonPortfolio */}
             <img src="./Assets/Images/CLR-Logo.png" alt="Portfolio-Logo"></img>
             <h1>Christina <span id="elle">l </span>. Robinson | Web Developer</h1>
         </div>
     );
 }
+
+export default Header;
---
+++ src/components/Project.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Project() {
+    return (
+        <div>
+            {/* copies in navigation code from portfolio already created https://github.com/ChannellNumber5/CRobinsonPortfolio */}
+            <div class="ImageAndCaption">
+            <a href="https://channellnumber5.github.io/CR-WeatherDashboard/" target="no_blank"><img src="./Assets/Images/CR-WeatherDashboardScreenShot.png" alt="Project Screenshot 4" class="Project-Images"></img></a>
+            <h3><strong>Weather Dashboard Application</strong></h3>
+            </div>
+        </div>
+    );
+}
+
+export default Project;
