095126bf958c7242d595e7b2576ab39038343fd6
Christina R <xsnachannell@gmail.com>
2022-09-10 09:19:44 -0700

    updates AboutMe.js with image placeholder and bio paragraph and updates Project.js with additional projects. still need to put in project images and hrefs

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -3,7 +3,13 @@ import React from 'react';
 function AboutMe() {
     return (
         <div>
-            <p>Just a little about me!</p>
+            <div id="developerPhotoEl">
+                <img src="">Developer Photo</img>
+            </div>
+            <div id="aboutMeText">
+                <h3>Just a little about me!</h3>
+                <p>I'm an east coast born, west coast based professional embarking on a career shift into tech and web development. After graduating from Carnegie Mellon University with my bachelors in civil engineering, I pursued a career in the field of nuclear engineering. After a few years and a few qualifications later, I realized it wasn't quite for me so I transitioned into working with our new engineers and managing the day-to-day of our engineering recruiting program and never really looked back. Until now... tHough I love what I do, I've missed the more technical aspects of engineering and problem solving and, long story short, I found myself on this journey to becoming a full stack web developer.If you want to follow my journey and see how far I progress, be sure to favorite my portfolio and check back in for updates!</p>
+            </div>
         </div>
     );
 }
--- src/components/Project.js
+++ src/components/Project.js
@@ -6,6 +6,26 @@ function Project() {
             {/* copies in project code from portfolio already created https://github.com/ChannellNumber5/CRobinsonPortfolio */}
             <div className="ImageAndCaption">
             {/* <a href="https://channellnumber5.github.io/CR-WeatherDashboard/" target="no_blank"><img src="./public/CR-WeatherDashboardScreenShot.png" alt="Project Screenshot 4" className="Project-Images"></img></a> */}
+            <h3><strong>Array App</strong></h3>
+            </div>
+            <div className="ImageAndCaption">
+            {/* <a href="https://channellnumber5.github.io/CR-WeatherDashboard/" target="no_blank"><img src="./public/CR-WeatherDashboardScreenShot.png" alt="Project Screenshot 4" className="Project-Images"></img></a> */}
+            <h3><strong>The Taste &amp; Sip</strong></h3>
+            </div>
+            <div className="ImageAndCaption">
+            {/* <a href="https://channellnumber5.github.io/CR-WeatherDashboard/" target="no_blank"><img src="./public/CR-WeatherDashboardScreenShot.png" alt="Project Screenshot 4" className="Project-Images"></img></a> */}
+            <h3><strong>DEVise</strong></h3>
+            </div>
+            <div className="ImageAndCaption">
+            {/* <a href="https://channellnumber5.github.io/CR-WeatherDashboard/" target="no_blank"><img src="./public/CR-WeatherDashboardScreenShot.png" alt="Project Screenshot 4" className="Project-Images"></img></a> */}
+            <h3><strong>TechBlog</strong></h3>
+            </div>
+            <div className="ImageAndCaption">
+            {/* <a href="https://channellnumber5.github.io/CR-WeatherDashboard/" target="no_blank"><img src="./public/CR-WeatherDashboardScreenShot.png" alt="Project Screenshot 4" className="Project-Images"></img></a> */}
+            <h3><strong>Text Editor PWA</strong></h3>
+            </div>
+            <div className="ImageAndCaption">
+            <a href="https://channellnumber5.github.io/CR-WeatherDashboard/" target="no_blank"><img src="./public/CR-WeatherDashboardScreenShot.png" alt="Project Screenshot 4" className="Project-Images"></img></a>
             <h3><strong>Weather Dashboard Application</strong></h3>
             </div>
         </div>
